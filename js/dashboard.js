f2a49198eff9bd213a56e5cc1663154f89be07cf
Victor Adeleye <victor.adeleye@globalaccelerex.com>
2019-11-05 10:57:14 +0000

    first commit

---
+++ js/dashboard.js
@@ -0,0 +1,10 @@
+function openSlideMenu() {
+    document.getElementById('dside-menu').style.width = '250px';
+    document.getElementById('dside-menu').style.height = '100%';
+}
+function closeSlideMenu() {
+    document.getElementById('dside-menu').style.width = '0';
+}
+function openInvestment() {
+    document.getElementById('mylist').style.display = 'none';
+}
